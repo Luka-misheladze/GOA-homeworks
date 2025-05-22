fbeb28164eae892d1ba99f5892ee073376e4ae3a
Luka-misheladze <qweqwe@gmail.com>
2025-05-22 19:15:14 +0400

    Homework

---
+++ Day 04/Homework/index.py
@@ -0,0 +1,59 @@
+#                                                                                   დავალება N3:
+
+
+Name = "luka"
+Year = 2010
+
+print(Name+ " was born in " +str(Year) + " he is turning 15 this year")
+print(f"{Name} was born in {Year} he is turning 15 this year")
+
+
+
+#                                            მას ეს იუთუბიდან ვნახე იმიტომ რომ არ ერორს მიწერდა, არ შეიძლებოდა სტრინგი ინტეჯერთან ერთად.
+
+#                                                        არ ვიცოდი რა მექნა და იუთუბიდან დავიხმარე იმედია პრობლემა არ იქნება :)
+ 
+
+#                                      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#                                                                                    დავალება N4:
+#                                                                      გამოვთვალოთ მართკუთხედის ფართობი:
+
+
+lenght = 9.7
+width = 5.2
+Result = lenght*width
+print(Result)
+
+
+#                                                                                    დავალება N5: 
+
+Item = "bike"
+price = 270
+
+print("Luka wants to buy a bike it's price is " +str(price) + " but he still can't buy the bike he wants because he doesnt have enough money for it")
+
+
+
+
+
+#                                                                                    დავალება N6:
+
+
+
+int1 = 414
+int2 = 152
+
+Result = int1+int2
+print(Result)
+
+
+#                                                                                    დავალება N7:
+
+
+
+inte1 = 60
+inte2 = 47
+Result = inte1 / inte2
+print(Result)
