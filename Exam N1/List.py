414dac54992b6a3083919f5b4976510d7e96e2ee
Luka-misheladze <qweqwe@gmail.com>
2025-07-26 15:19:12 +0400

    3 saitii gavaketeee calke ragaca Exam damawerines

---
+++ Exam N1/List.py
@@ -0,0 +1,15 @@
+print("")
+print("Before : ")
+
+foods = ["Cow" , "Pig" , 44 , 44.5 , True]
+print(foods)
+
+foods.append(False)
+foods.insert(2 , "Nia")
+
+print("")
+
+print("After : ")
+print(foods)
+
+print("")
---
+++ Exam N1/Loop.py
@@ -0,0 +1,24 @@
+# ეს არის While loop :
+
+i = 0
+while i < 11:
+    print(i)
+    i = i + 1
+
+print("")
+
+print("This is While loop.")
+
+print("")
+
+# # ეს არის For loop (cycle) :
+
+for i in range (11):
+    print(i)
+
+
+print("")
+
+print("This is For loop (cycle)")
+
+print("")
