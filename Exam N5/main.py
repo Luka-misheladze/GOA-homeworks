a6851327862d1e2029b55a22db36136075e0ef74
Luka-misheladze <misheladzeluka@gmail.com>
2025-11-08 20:45:10 +0400

    Exam N5

---
+++ Exam N5/main.py
@@ -0,0 +1,120 @@
+# age = 15
+# print(age)
+
+# num = 12
+# num1 = 71
+
+# print(num + num1)
+
+# name = "Luka"
+# print(name)
+
+# name2 = "gio"
+# print(name + name2)
+
+# number = 18.9
+# print(number)
+
+# number1 = 14
+# number2 = 15.5
+
+# answer = number1 * number2
+# print(answer)
+# print(answer**2)
+
+# name = "luka"
+# surname = "misheladze"
+
+# print("ჩემი სახელია:" , name , "ჩემი გვარია:" , surname)
+
+# name = input("Please enter your name: ")
+# surname = input("Please enter your surname: ")
+
+# print("hello" , name , surname)
+
+# age = 15
+# print("მე ვარ: " + str(age) , "წლის")
+
+# x = int(input("Please enter a number: "))
+
+# print(x**2)
+
+# >
+# <
+# ==
+# >=
+# <=
+# !=
+# bool
+
+# print(False or True)
+
+# age = int(input("please enter your age: "))
+
+# if age >= 18:
+#     print("ur an adult")
+# else:
+#     print("your underage")
+
+# მომხმარებელს შემოვატანინოთ ქულა 1 და ასამდე. თუ ქულა არის 80 სა და 100 ს შორის გამოიტანე "A".
+#  თუ ქულა არის 60 სა და 79 ს შორის "B".
+#  თუ 40 სა და 59 ს შორის "C".
+#  სხვა შემთხვევაში D
+
+# grade = int(input("Please enter your grade: "))
+
+# if grade >= 80 and grade <= 100:
+#     print("A")
+# elif grade >= 60 and grade <= 79:
+#     print("B")
+# elif grade >= 40 and grade <= 59:
+#     print("C")
+# else:
+#     print("D")
+
+
+#           start end stop
+# for i in range(1, 10, 1):
+#     print(i)
+
+# for i in range(10, 100):
+#     print(i)
+
+# for i in range(0, 102, 2):
+#     print(i)
+
+# for i in range(10, -1, -1):
+#     print(i)
+
+# name = input("please enter your name: ")
+
+# for i in range(10):
+#     name = input("please enter your name: ")
+
+# age = 0
+
+# while age <= 18:
+#     print(age)
+#     age += 1
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# # print(numbers[5])
+# # print(numbers)
+# numbers[5], numbers[3] = numbers[3], numbers[5]
+# # print(numbers)
+
+# numbers[2] = "luka"
+# print(numbers)
+
+# print(numbers[2:5])
+# print(numbers[1:7:2])
+# print(numbers[::-1])
+# print(numbers[::-3])
+
+# print(numbers.pop(6))
+
+# for num in numbers:
+#     if num % 2 == 0:
+#         print("ლუწი")
+#     else:
+#         print("კენტი")
